626fd9555988dc344197db742b6d11e4fb9b4d9c
Angel Archbold <angelarchbold09@gmail.com>
2020-07-23 22:21:56 -0500

    firts  commit, auth with google

---
+++ index.js
@@ -0,0 +1,174 @@
+const express = require("express");
+const passport = require("passport");
+const boom = require("@hapi/boom");
+const cookieParser = require("cookie-parser");
+const axios = require("axios");
+const { config } = require("./config");
+
+const app = express();
+
+//body parser
+app.use(express.json());
+app.use(cookieParser());
+
+//basic strategi
+require("./utils/auth/strategies/basic");
+//oauth
+require("./utils/auth/strategies/oauth");
+//google
+require("./utils/auth/strategies/google");
+
+// var time
+const THIRD_DAYS_IN_SEC = 2592000000;
+const TWO_HOURS_IN_SEC = 7200000;
+
+app.post("/auth/sign-in", async (req, res, next) => {
+  const { rememberMe } = req.body;
+  passport.authenticate("basic", (error, data) => {
+    try {
+      if (error || !data) {
+        boom.unauthorized();
+      }
+
+      req.login(data, { session: false }, async (error) => {
+        if (error) {
+          next(error);
+        }
+
+        const { token, ...user } = data;
+
+        res.cookie("token", token, {
+          httpOnly: !config.dev,
+          secure: !config.dev,
+          maxAge: rememberMe ? THIRD_DAYS_IN_SEC : TWO_HOURS_IN_SEC
+        });
+
+        res.status(200).json(user);
+      });
+    } catch (error) {
+      next(error);
+    }
+  })(req, res, next);
+});
+
+app.post("/auth/sign-up", async (req, res, next) => {
+  const { body: user } = req;
+
+  try {
+    await axios({
+      url: `${config.apiUrl}/api/auth/sign-up`,
+      method: "post",
+      data: user
+    });
+
+    res.status(201).json({ message: "user created" });
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.get("/movies", (req, res, next) => {});
+
+app.post("/user-movies", async (req, res, next) => {
+  try {
+    const { body: userMovie } = req;
+    const { token } = req.cookies;
+
+    const { data, status } = await axios({
+      url: `${config.apiUrl}/api/user-movies`,
+      headers: { Authorization: `Bearer ${token}` },
+      method: "post",
+      data: userMovie
+    });
+
+    if (status !== 201) {
+      return next(boom.badImplementation());
+    }
+
+    res.status(201).json(data);
+  } catch (error) {
+    next(error.message);
+  }
+});
+
+app.delete("/user-movies/:userMovieId", async (req, res, next) => {
+  try {
+    const { userMovieId } = req.params;
+
+    const { token } = req.cookies;
+
+    const { data, status } = await axios({
+      url: `${config.apiUrl}/api/user-movies/${userMovieId}`,
+      headers: { Authorization: `Bearer ${token}` },
+      method: "delete"
+    });
+
+    if (status !== 200) {
+      return next(boom.badImplementation());
+    }
+
+    res.status(200).json(data);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.get(
+  "/auth/google-oauth",
+  passport.authenticate("google-oauth", {
+    scope: ["email", "profile", "openid"]
+  })
+);
+
+app.get(
+  "/auth/google-oauth/callback",
+  passport.authenticate(
+    "google-oauth",
+    { session: false }),
+
+    async (req, res, next) => {
+      if(!req.user){
+        next(boom.unauthorized())
+      }
+
+      const {token, ...user} = req.user
+
+      res.cookie('token',token, {
+        httpOnly:!config.dev,
+        secure:!config.dev
+      })
+
+      res.status(200).json(user)
+    }
+);
+
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["email", "profile", "openid"]
+  })
+);
+
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", { session: false }),
+  function(req, res, next) {
+    if (!req.user) {
+      next(boom.unauthorized());
+    }
+
+    const { token, ...user } = req.user;
+
+    res.cookie("token", token, {
+      httpOnly: !config.dev,
+      secure: !config.dev
+    });
+
+    res.status(200).json(user);
+  }
+);
+
+app.listen(config.port, () => {
+  console.log(`listening http://localhost:${config.port}`);
+});
